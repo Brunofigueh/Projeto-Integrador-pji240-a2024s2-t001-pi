37de793f96c9bc79b1520a5063b3a50e2276df13
brunofigueh <bruno_alqui@hotmail.com>
2024-11-14 17:08:30 -0300

    fix v6

--- conexao.php
+++ conexao.php
@@ -3,7 +3,7 @@
     $servidor = 'mysql://hov9byvj7u6tdy6x:yd785ippneanbe8w@izm96dhhnwr2ieg0.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/mrrrh8fsanxe85db'; // ou o endereço do seu servidor de banco de dados
     $usuario = 'hov9byvj7u6tdy6x'; // seu usuário do MySQL
     $senha = 'yd785ippneanbe8w'; // sua senha do MySQL
-    $dbname = 'cadastro'; // o nome do seu banco de dados
+    $dbname = 'mrrrh8fsanxe85db'; // o nome do seu banco de dados
 
     // Cria a conexão
     $conexao = new mysqli($servidor, $usuario, $senha, $dbname);
