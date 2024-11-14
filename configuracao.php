ffc119d76c1b2b3cd643711902671a6f17fcbe49
brunofigueh <bruno_alqui@hotmail.com>
2024-11-14 15:09:52 -0300

    fix v1

--- configuracao.php
+++ configuracao.php
@@ -1,6 +1,6 @@
 <?php
 
-    $servidor = 'localhost'; // ou o endereço do seu servidor de banco de dados
+    $servidor = 'localhosthttps://pji240-a2024s2-t001-pi-f1d850c98e99.herokuapp.com/'; // ou o endereço do seu servidor de banco de dados
     $usuario = 'root'; // seu usuário do MySQL
     $senha = ''; // sua senha do MySQL
     $dbname = 'cadastro'; // o nome do seu banco de dados
